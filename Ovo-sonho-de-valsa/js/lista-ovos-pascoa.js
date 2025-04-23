f67c07aa709df69bb7771a74d8389145ae139e7f
orbegel <orbegeel@gmail.com>
2025-04-22 22:03:23 -0300

    Add files via upload

---
+++ Ovo-sonho-de-valsa/js/lista-ovos-pascoa.js
@@ -0,0 +1,63 @@
+// Array de produtos de Ovos de Páscoa
+const ovosDePascoa = [
+  {
+    id: 1,
+    nome: "Ovo de Páscoa Lacta Barbie",
+    descricao: "Delicioso ovo de chocolate ao leite Lacta tema Barbie",
+    peso: "166g",
+    preco: 49.90,
+    categoria: "infantil",
+    imagem: "images/ovo-de-pascoa-lacta-barbie-166g.jpg",
+    url: "produto?id=1"
+  },
+  {
+    id: 2,
+    nome: "Ovo de Páscoa Lacta Diamante Negro",
+    descricao: "Ovo de chocolate meio amargo Diamante Negro da Lacta",
+    peso: "176g", // Peso aproximado, ajuste se necessário
+    preco: 52.90,
+    categoria: "adulto",
+    imagem: "images/ovo-de-pascoa-lacta-diamante-negro.jpg",
+    url: "produto?id=2"
+  },
+  {
+    id: 3,
+    nome: "Ovo de Páscoa Lacta Hotwheels",
+    descricao: "Ovo de chocolate ao leite Lacta tema Hotwheels para crianças",
+    peso: "170g", // Peso aproximado, ajuste se necessário
+    preco: 49.90,
+    categoria: "infantil",
+    imagem: "images/ovo-de-pascoa-lacta-hotwheels.jpg",
+    url: "produto?id=3"
+  },
+  {
+    id: 4,
+    nome: "Ovo de Páscoa Lacta ao Leite",
+    descricao: "Tradicional ovo de Páscoa Lacta de chocolate ao leite",
+    peso: "170g",
+    preco: 45.90,
+    categoria: "tradicional",
+    imagem: "images/ovo-de-pascoa-lacta-ao-leite-170g.jpg",
+    url: "produto?id=4"
+  },
+  {
+    id: 5,
+    nome: "Ovo de Páscoa Ouro Branco",
+    descricao: "Ovo de Páscoa sabor chocolate branco Ouro Branco da Lacta",
+    peso: "359g",
+    preco: 69.90,
+    categoria: "premium",
+    imagem: "images/ovo-de-pascoa-ouro-branco-359g.jpg",
+    url: "produto?id=5"
+  },
+  {
+    id: 6,
+    nome: "Ovo de Páscoa Bis ao Leite",
+    descricao: "Ovo de Páscoa recheado com chocolate Bis ao leite",
+    peso: "318g",
+    preco: 59.90,
+    categoria: "recheado",
+    imagem: "images/ovo-de-pascoa-bis-ao-leite-318g.jpg",
+    url: "produto?id=6"
+  }
+];
